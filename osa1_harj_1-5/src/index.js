0943976ebe80f5a7059ae520ccf026cdf0987b39
Juha <juha.kangas@welho.com>
2018-01-18 19:22:08 +0200

    Tehtävä 1.2

--- osa1_harj_1-5/src/index.js
+++ osa1_harj_1-5/src/index.js
@@ -5,10 +5,14 @@ const Otsikko = (props) => (<h1>{props.kurssi}</h1>)
 
 const Sisalto = (props) => (
   <div>
-    <p>{props.osa1} {props.tehtavia1}</p>
-    <p>{props.osa2} {props.tehtavia2}</p>
-    <p>{props.osa3} {props.tehtavia3}</p>
+    <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+    <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+    <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
   </div>
+)
+
+const Osa = (props) => (
+  <p>{props.osa} {props.tehtavia}</p>
 )
 
 const Yhteensa = (props) => (
