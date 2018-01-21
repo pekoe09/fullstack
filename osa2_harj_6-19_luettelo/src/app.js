fe930708607d8d53f5b68a29fe9b3dbdb41f2a9d
Juha <juha.kangas@welho.com>
2018-01-21 18:35:34 +0200

    Tehtävä 2.6

---
+++ osa2_harj_6-19_luettelo/src/app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: this.state.newName }
+    const newPersons = this.state.persons.concat(newPerson)
+    this.setState({
+      persons: newPersons,
+      newName: ''
+    })
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ newName: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addPerson}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        {this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}
+      </div>
+    )
+  }
+}
+
+export default App
