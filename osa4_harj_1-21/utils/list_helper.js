b60f3b8224638c441ce6bb7ceca1664921832fcb
Juha <juha.kangas@welho.com>
2018-01-30 23:50:27 +0200

    Tehtävä 4.4

--- osa4_harj_1-21/utils/list_helper.js
+++ osa4_harj_1-21/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs && blogs.length > 0) {
+    return blogs.reduce((totalLikes, blog) => {
+      return blog.likes + totalLikes
+    }, 0)
+  } else {
+    return 0
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
