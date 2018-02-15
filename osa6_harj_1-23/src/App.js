2eab31e26e2fab9ddb85d4c9be66a6e00e884284
Juha <juha.kangas@welho.com>
2018-02-15 23:02:14 +0200

    Tehtävä 6.6

--- osa6_harj_1-23/src/App.js
+++ osa6_harj_1-23/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Notification from './components/Notification'
+import ConnectedNotification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div>
         <h1>Programming anecdotes</h1>
         <Filter store={this.props.store} />
-        <Notification store={this.props.store} />
+        <ConnectedNotification store={this.props.store} />
         <AnecdoteList store={this.props.store} />
         <AnecdoteForm store={this.props.store} />
       </div>
