a40c358069664b8d723ad1b1ef28f72883d62795
Juha <juha.kangas@welho.com>
2018-02-28 22:08:27 +0200

    Tehtävä 7.6 oikeesti

--- osa7_harj_1-23/react-ui/src/components/Blog.js
+++ osa7_harj_1-23/react-ui/src/components/Blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Blog= ({ blog, user, handleLike, handleDelete }) => {
+const Blog = ({ blog, user, handleLike, handleDelete }) => {
   return (
     <div>
       <h2>{blog.title} {blog.author}</h2>
@@ -13,6 +13,11 @@ const Blog= ({ blog, user, handleLike, handleDelete }) => {
       {!blog.user && <div>added by unknown</div>}
       {(!blog.user || blog.user.username === user.username)
         && <div><button onClick={handleDelete}>delete</button></div>
+      }
+      <h3>Comments</h3>
+      {blog.comments.length === 0
+        ? <div>There are no comments yet</div>
+        : <ul>{blog.comments.map(c => <li key={c}>{c}</li>)}</ul>
       }
     </div>
   )
